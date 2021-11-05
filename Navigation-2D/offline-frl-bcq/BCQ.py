a60fe0a94a7c0b29ab2f978bc3c5674725fcd318
jueun-park <pjueun08@gmail.com>
2021-11-05 15:35:32 +0900

    save and load

--- Navigation-2D/offline-frl-bcq/BCQ.py
+++ Navigation-2D/offline-frl-bcq/BCQ.py
@@ -190,3 +190,25 @@ class BCQ(object):
 
 			for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
 				target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+	def save(self, filename):
+		torch.save(self.critic.state_dict(), filename + "_critic")
+		torch.save(self.critic_optimizer.state_dict(), filename + "_critic_optimizer")
+		
+		torch.save(self.actor.state_dict(), filename + "_actor")
+		torch.save(self.actor_optimizer.state_dict(), filename + "_actor_optimizer")
+
+		torch.save(self.vae.state_dict(), filename + "_vae")
+		torch.save(self.vae_optimizer.state_dict(), filename + "_vae_optimizer")
+
+	def load(self, filename):
+		self.critic.load_state_dict(torch.load(filename + "_critic"))
+		self.critic_optimizer.load_state_dict(torch.load(filename + "_critic_optimizer"))
+		self.critic_target = copy.deepcopy(self.critic)
+
+		self.actor.load_state_dict(torch.load(filename + "_actor"))
+		self.actor_optimizer.load_state_dict(torch.load(filename + "_actor_optimizer"))
+		self.actor_target = copy.deepcopy(self.actor)
+		
+		self.vae.load_state_dict(torch.load(filename + "_vae"))
+		self.vae_optimizer.load_state_dict(torch.load(filename + "_vae_optimizer"))
